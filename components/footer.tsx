82f03602434a280fd32e0264f3210d285519fc97
Godfrey Onyango <godfreyonyango365@gmail.com>
2026-07-12 01:07:17 +0300

    Initial commit

--- components/footer.tsx
+++ components/footer.tsx
@@ -92,10 +92,13 @@ export default function Footer() {
               {[
                 { name: "EGW Writings", url: "https://egwwritings.org/" },
                 { name: "Sunday Law", url: "https://SundayLaw.com/" },
+                  { name: "Old SDA Organization", url: "https://oldsda.org/" },
                 { name: "Young Evangelists Ministry", url: "https://youngevangelists.vercel.app/" },
                 { name: "Advent AI", url: "https://adventai.app/" },
                 { name: "Advent Nurutech", url: "https://www.adventnurutech.xyz/" },
                 { name: "Nuru Shop", url: "https://nurushop.co.ke/" },
+                { name: " Remsam Homestade", url: "https://https://remsamhomestead.com//" },
+                 { name: " Gospel Sounders Ministry", url: "https://www.gspublicationsandmissions.org//" },
               ].map((site, index) => (
                 <a key={index} href={site.url} target="_blank" rel="noopener noreferrer" className="block p-4 rounded-xl bg-gray-800/40 hover:bg-blue-600/20 border border-gray-700 hover:border-blue-500 transition-all duration-300 group">
                   <div className="flex items-center gap-3">
