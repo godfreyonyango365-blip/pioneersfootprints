45778eaf7d452bb9ab69d9fff9e7c1be4d22d6b0
Godfrey Onyango <godfreyonyango365@gmail.com>
2026-07-31 06:45:01 +0300

    Update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -97,7 +97,7 @@ export default function Footer() {
                 { name: "Advent AI", url: "https://adventai.app/" },
                 { name: "Advent Nurutech", url: "https://www.adventnurutech.xyz/" },
                 { name: "Nuru Shop", url: "https://nurushop.co.ke/" },
-                { name: " RemSam Homestade", url: "https://remsamhomestead.com//" },
+                { name: " RemSam Homestead", url: "https://remsamhomestead.com//" },
                  { name: " Gospel Sounders Ministry", url: "https://www.gspublicationsandmissions.org/" },
               ].map((site, index) => (
                 <a key={index} href={site.url} target="_blank" rel="noopener noreferrer" className="block p-4 rounded-xl bg-gray-800/40 hover:bg-blue-600/20 border border-gray-700 hover:border-blue-500 transition-all duration-300 group">
